8814b4009cf6ec2a907383870c1e4fa8f099a72a
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2023-09-11 00:08:58 +0100

    made the upload functionality work

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -5,56 +5,96 @@ import { UilScenery } from '@iconscout/react-unicons';
 import { UilPlayCircle } from '@iconscout/react-unicons';
 import { UilLocationPoint } from '@iconscout/react-unicons';
 import { UilSchedule } from '@iconscout/react-unicons';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadImage } from '../../actions/uploadAction';
 
 
 
 const PostShare = () => {
+  const { user } = useSelector(state => state.authReducer.authData);
+  const dispatch = useDispatch();
   const [image, setImage] = useState(null);
   const imageRef = useRef(); 
+  const desc = useRef();
 
   const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
       console.log(img)
-      setImage({
-        image: URL.createObjectURL(img),
-      });
+      setImage(img);
     }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value
+    }
+
+    if (image) {
+      const data = new FormData();
+      const filename = Date.now() + image.name;
+      data.append('name', filename);
+      data.append('file', image);
+      newPost.image = filename;
+
+      try {
+        dispatch(uploadImage(data));
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    
+    dispatch(uploadPost(newPost));
+
   }
 
   return (
     <div className="PostShare">
-        <img src={ProfileImage} alt="" />    
-        <div>
-            <input type="text" placeholder="What is happening?" />
-            <div className="postOptions">
-              <div className="option" style={{color: 'var(--photo)'}} onClick={() => imageRef.current.click( )}>
-                <UilScenery />  Photo
-              </div>
-              <div className="option" style={{color: 'var(--video)'}}>
-                <UilPlayCircle />Video
-              </div>
-              <div className="option" style={{color: 'var(--location)'}}>
-                  <UilLocationPoint />Location
-              </div>
-              <div className="option" style={{color: 'var(--schedule)'}}>
-                 <UilSchedule /> Schedule
-            </div>
-          <button className="button ps-button">Share</button>
-          <div style={{display: 'none'}}>
-            <input type='file' name='myImage' ref={imageRef} onChange={onImageChange}/>
+      <img src={ProfileImage} alt="" />
+      <div>
+        <input type="text" placeholder="What is happening?" ref={desc} required/>
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
+            <UilScenery /> Photo
+          </div>
+          <div className="option" style={{ color: "var(--video)" }}>
+            <UilPlayCircle />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <UilLocationPoint />
+            Location
+          </div>
+          <div className="option" style={{ color: "var(--schedule)" }}>
+            <UilSchedule /> Schedule
+          </div>
+          <button className="button ps-button" onClick={handleSubmit}>Share</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
           </div>
         </div>
-        
+
         {image && (
-          <div className='previewImage'>
+          <div className="previewImage">
             <p onClick={() => setImage(null)}>X</p>
-            <img src={image.image} alt='' />
+            <img src={URL.createObjectURL(image)} alt="" />
           </div>
         )}
-        </div>  
+      </div>
     </div>
-  )
+  );
 }
 
 export default PostShare
